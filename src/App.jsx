815d574cdd267b8c98a136fdd3c9d798202afbb9
Mehedi Hasan <mehedih.dev@gmail.com>
2023-05-07 08:02:19 +0600

    added new file called Entry.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import emojipedia from './components/emojipedia'
 
 function App() {
   return (
@@ -8,18 +9,7 @@ function App() {
       </h1>
 
       <dl className="dictionary">
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              💪
-            </span>
-            <span>Tense Biceps</span>
-          </dt>
-          <dd>
-            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-            used in connection with doing sports, e.g. at the gym.
-          </dd>
-        </div>
+        
         <div className="term">
           <dt>
             <span className="emoji" role="img" aria-label="Tense Biceps">
